6c31e4dd2f88d112b1593d1e749697dbb9e88229
jonpfluger <jonpfluger718@gmail.com>
2022-10-24 21:42:12 -0500

    inquirer

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+import inquirer from 'inquirer'
 import chalkAnimation from "chalk-animation"
 import { generateSlug } from 'random-word-slugs'
 
-const length = parseInt(process.argv[2])
+inquirer
+    .prompt([
+        {
+            type: "number",
+            name: "length",
+            message: "How long should the band name be?",
+            default: 3,
+        },
+        {
+            type: 'list',
+            name: "animationName",
+            message: "Which animation would you like to use?",
+            default: "rainbow",
+            choices: ["rainbow", "pulse", "glitch", "neon", "radar", "karaoke"],
+        },
+    ])
+    .then((answers) => {
+        console.log(answers)
 
-const bandName = generateSlug(length, { format: "title" })
+        const bandName = generateSlug(answers.length, { format: "title" })
 
-chalkAnimation.rainbow(bandName)
+        if (answers.animationName === "pulse") {
+            chalkAnimation.pulse(bandName)
+        } else if (answers.animationName === "rainbow") {
+            chalkAnimation.rainbow(bandName)
+        } else if (answers.animationName === "radar") {
+            chalkAnimation.radar(bandName)
+        } else if (answers.animationName === "neon") {
+            chalkAnimation.neon(bandName)
+        } else if (answers.animationName === 'glitch') {
+            chalkAnimation.glitch(bandName)
+        }
+
+
+    })
+    .catch(error => console.log(error))
+
+
+
+
+
+
+
+
+
+
